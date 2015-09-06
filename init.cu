83bc52cce29b770032acadb2e73defa9308a2a82
viorik <vpatrauc@gmail.com>
2015-09-06 20:24:20 +0100

    fixed symbolic links

--- init.cu
+++ init.cu
@@ -5,11 +5,11 @@
 
 LUA_EXTERNC DLL_EXPORT int luaopen_libunsupgpu(lua_State *L);
 
-int luaopen_libundupgpu(lua_State *L)
+int luaopen_libunsupgpu(lua_State *L)
 {
   lua_newtable(L);
   unsupgpu_util_init(L);
-  lua_setfield(L, LUA_GLOBALSINDEX, "unsupgpu");
+  //lua_setfield(L, LUA_GLOBALSINDEX, "unsupgpu");
   return 1;
 }
 
